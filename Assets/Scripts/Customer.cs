e7163357e08dee482f3d948291ece7dfe24a1f45
Reynolds398 <reynoldladestu@gmail.com>
2022-07-15 16:32:43 -0700

    Added Customer Animation
    Also updated the Customer script in order for it to be able to switch between animation states

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    private Animator customerAnim;
+    [SerializeField] private bool satisfied;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        customerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (satisfied)
+        {
+            customerAnim.SetTrigger("Leave");
+        }
     }
 }
