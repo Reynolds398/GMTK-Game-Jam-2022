68f509c681b5a696a527c23cb80c8cfa4406af8d
Reynolds398 <reynoldladestu@gmail.com>
2022-07-16 15:40:04 -0700

    Updated the Arrow Buttons
    Now arrow buttons change number when pressed

--- Assets/Scripts/ArrowButtons.cs
+++ Assets/Scripts/ArrowButtons.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ArrowButtons : MonoBehaviour
 {
     private bool held = false;
+    private int max = 5;
+    private static int count = 0;
+
+    [SerializeField] private int id;
     [SerializeField] private GameObject unclickedButton;
+    [SerializeField] private TextMeshPro amountText;
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +38,17 @@ public class ArrowButtons : MonoBehaviour
     private void OnMouseUp()
     {
         held = false;
+
+        if (id == 1 && count < max) // Up Arrow
+        {
+            count++;
+        }
+
+        if (id == 2 && count > 0) // Down Arrow
+        {
+            count--;
+        }
+
+        amountText.SetText(count.ToString());
     }
 }
