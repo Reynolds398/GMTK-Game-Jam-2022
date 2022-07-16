c49002ad65c355b4b318bd070863759717793008
Reynolds398 <reynoldladestu@gmail.com>
2022-07-16 11:55:16 -0700

    Made the Arrow Button working

--- Assets/Scripts/ArrowButtons.cs
+++ Assets/Scripts/ArrowButtons.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ArrowButtons : MonoBehaviour
 {
+    private bool held = false;
+    [SerializeField] private GameObject unclickedButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class ArrowButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!held)
+        {
+            unclickedButton.SetActive(true);
+        }
         
+    }
+
+    private void OnMouseDrag()
+    {
+        held = true;
+        unclickedButton.SetActive(false);
+    }
+
+    private void OnMouseUp()
+    {
+        held = false;
     }
 }
